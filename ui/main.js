4e55fa0a2c01746cf8f0f48c79c7135d82e38f28
Sudhanshu Jain <36514625+blacksag@users.noreply.github.com>
2018-03-01 00:48:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,15 +10,17 @@ submit.onclick = function() {
     request.onreadystatechange = function() {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                
+            var names = ['Sudy' , 'Kittu'];
+            var list = '';
+            for (var i=0; i<names.length; i++) {
+                list+= '<li>' + names[i] + '</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;    
             }
         }
     };
-    var names = ['Sudy' , 'Kittu'];
-    var list = '';
-    for (var i=0; i<names.length; i++) {
-        list+= '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    
+    request.open('GET','http://sudhanshujain982.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null)
 };
