02bfe549ee2609d3c3815e9ed1f29483df59635f
Sudhanshu Jain <36514625+blacksag@users.noreply.github.com>
2018-03-01 01:05:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var names = [];
 app.get('/submit-name', function (req, res) {
   var name = req.query.name;
   
