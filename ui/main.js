046a96398683055f098819ac675ac404b484f0af
Sudhanshu Jain <36514625+blacksag@users.noreply.github.com>
2018-02-28 19:43:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
-console.log('Loaded!');
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('sbmt');
+submit.onclick = function() {
+    var names = {'name1','name2'};
+    var list = '';
+    for (var i=0; i<names.length; i++) {
+        list+= '<li>' + name[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ui.innerHTML=list
+}
